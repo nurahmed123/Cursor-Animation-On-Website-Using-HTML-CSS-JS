95a43e6927c100d0fcdc257f7a265f6afdc96bfe
Nur Ahmed <84973115+nurahmed123@users.noreply.github.com>
2022-03-10 00:13:52 +0600

    Cursor moving animation

---
+++ main.js
@@ -0,0 +1,6 @@
+let cursor = document.querySelector(".cursor");
+let cursorInner = document.querySelector(".cursorInner");
+
+document.addEventListener("mousemove", (event) =>{
+    cursor.style.cssText = cursorInner.style.cssText = "left: "+ event.clientX + "px; top: " + event.clientY + "px;"
+});
